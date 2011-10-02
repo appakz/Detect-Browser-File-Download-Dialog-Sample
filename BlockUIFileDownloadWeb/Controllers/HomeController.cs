007d24119174b2d558a0744616b63f09954986c3
Jesse Taber <jesse.taber@gmail.com>
2011-10-02 11:49:21 -0400

    initial commit

---
+++ BlockUIFileDownloadWeb/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using BlockUIFileDownloadWeb.Models;
+
+namespace BlockUIFileDownloadWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "File Download With Block UI Sample";
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult CreateInfoSheet(CustomerInfoModel customerInfo)
+        {
+            //use the submitted download token value to set a cookie in the response
+            Response.AppendCookie(new HttpCookie("fileDownloadToken", customerInfo.DownloadToken));
+
+            var file = CreateFile();
+            var outputFileName = string.Format("{0}_CustomerInfo.pdf", DateTime.Now.Ticks);
+            return File(file, "application/pdf", outputFileName);
+        }
+
+        private Stream CreateFile()
+        {
+            //to simulate a server process taking a few seconds to render the PDF file:
+            Thread.Sleep(5000);
+            var filePath = Request.MapPath("~/Content/CustomerInfoSheet.pdf");
+            return new FileStream(filePath, FileMode.Open);
+        }
+    }
+}
---
+++ BlockUIFileDownloadWeb/Models/CustomerInfoModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlockUIFileDownloadWeb.Models
+{
+    public class CustomerInfoModel
+    {
+        public string DownloadToken { get; set; }
+    }
+}
